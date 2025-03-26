ef62b26943abc364c9292243a66fe377d2a049f6
Himanshukalika <himanshukalika75@gmail.com>
2025-03-26 10:34:57 +0530

    ...

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,7 +1,7 @@
-import { ClassValue, clsx } from 'clsx';
+import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 // Use any[] instead of ClassValue[] to avoid type issues
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+export function cn(...inputs: any[]) {
+  return twMerge(clsx(...inputs));
 } 
